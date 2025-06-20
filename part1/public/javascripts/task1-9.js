0cff29b1ba389dea8461cd37dc52c1587f4fb035
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 07:50:30 +0000

    [automatic commit] 2025-06-20 07:50:30 UTC 120.20.187.84

--- part1/public/javascripts/task1-9.js
+++ part1/public/javascripts/task1-9.js
@@ -6,7 +6,7 @@ createApp({
             dog_img_link: '',
             visible: false,
             button_pos: { top: 200, left: window.innerWidth - 200 / 2 - 75 }
-            move 
+            move_count : 0
         };
     },
     methods: {
