893828a6ee4d4e36bfccce61cd1feb4bf9df1276
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 05:32:36 +0000

    [automatic commit] 2025-06-20 05:32:36 UTC 120.20.187.84

--- part1/app.js
+++ part1/app.js
@@ -122,7 +122,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             AVG(ratings.rating) AS average_rating
         FROM Users u
         LEFT JOIN WalkRequests we ON wr.status = 'completed' AND wr.request_id IN (
-            SELECT wa.request_id FROM WalkApplication)`
+            SELECT wa.request_id FROM WalkApplications wa WHERE wa.walker_id = u.user_id AND wa.status = 'accepted')`
     }
 });
 
