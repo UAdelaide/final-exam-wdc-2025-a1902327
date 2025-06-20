c6fc3a79bfab521258ef091d86616a042b424339
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 10:59:29 +0000

    [automatic commit] 2025-06-20 10:59:29 UTC 120.20.137.139

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -44,7 +44,9 @@ router.post('/login', async (req, res) => {
 // POST logout
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
-    if (err) {}
+    if (err) {
+      return res.status(500).json({error: 'Can'})
+    }
   })
 })
 
