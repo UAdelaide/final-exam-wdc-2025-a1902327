795a45c425a2bc2e02261c95b4e88d0e30431e9a
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 07:38:07 +0000

    [automatic commit] 2025-06-20 07:38:07 UTC 120.20.187.84

--- part1/public/javascripts/task1-9.js
+++ part1/public/javascripts/task1-9.js
@@ -14,32 +14,33 @@ createApp({
             const xhr = new XMLHttpRequest();
 
             xhr.onload = () => {
-                try {
-                    const data = JSON.parse(xhr.responseText);
+                if (xhr.status === 200) {
+                    try {
+                        const data = JSON.parse(xhr.responseText);
 
-                    if (data.status === 'success') {
-                        this.dog_img_link = data.message;
+                        if (data.status === 'success') {
+                            this.dog_img_link = data.message;
+                        }
+                    } catch (e) {
+                        console.error("Error parsing JSON:", e);
                     }
-                } catch (e) {
-                    console.error("Error parsing JSON:", e);
+                } else {
+                    console.error("Request failed with status:", xhr.status);
                 }
-            } else {
-                console.error("Request failed with status:", xhr.status);
             }
-}
-},
+        },
 
-    showDogPics() {
-    this.visible = true;
-},
+        showDogPics() {
+            this.visible = true;
+        },
 
-    moveButton() {
-    var top = Math.random() * (window.innerHeight - 100);
-    var left = Math.random() * (window.innerWidth - 200);
-    this.button_pos = { top: top, left: left };
-}
+        moveButton() {
+            var top = Math.random() * (window.innerHeight - 100);
+            var left = Math.random() * (window.innerWidth - 200);
+            this.button_pos = { top: top, left: left };
+        }
     },
     mounted() {
-    this.getDog();
-}
+        this.getDog();
+    }
 }).mounted('#app');
