4e7ebdead259c038e187e19bd0fe78ec732ba758
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 05:38:06 +0000

    [automatic commit] 2025-06-20 05:38:06 UTC 120.20.187.84

--- part1/app.js
+++ part1/app.js
@@ -71,6 +71,7 @@ var checkDbConnection = async () => {
 };
 
 // API Routes
+app.use('/api', checkDbConnection);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
