753feb41382c8da9bf7108bda3da702d480ba1ec
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 03:50:51 +0000

    [automatic commit] 2025-06-20 03:50:51 UTC 120.20.187.84

--- part1/task1-5.sql
+++ part1/task1-5.sql
@@ -13,4 +13,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 INSERT INTO Dogs (name, size, owner_id) VALUES
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
 ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
-('Tow', 'large', (SELECT user_id FROM Users WHERE username = 'bobwalker')
+('Tow', 'large', (SELECT user_id FROM Users WHERE username = 'bobwalker')),
+('Rex', 'medium', (SELECT user_id FROM Users WHERE username = tommytung'))
