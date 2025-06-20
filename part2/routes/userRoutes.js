20784f21786d2033fc172ffe1fa88f7f68d44c0c
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 10:50:53 +0000

    [automatic commit] 2025-06-20 10:50:53 UTC 120.20.137.139

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,10 @@ router.post('/login', async (req, res) => {
 
     // Create a session for the user
     req.session.user = {
-      user_id: user.user
+      user_id: user.user_id,
+      username: user.username,
+      role: user.role
+    };
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
