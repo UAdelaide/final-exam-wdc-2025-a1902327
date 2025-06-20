f76e80c2c6d6079f1553423d87ac645d428af17b
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 05:12:20 +0000

    [automatic commit] 2025-06-20 05:12:20 UTC 120.20.187.84

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql');
+var mysql = require('mysql2/promise');
 var fs = require('fs');
 
 var indexRouter = require('./routes/index');
