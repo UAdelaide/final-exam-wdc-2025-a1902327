40040751430c03cb2ba5285594bff5a4a6d7161e
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 10:33:44 +0000

    [automatic commit] 2025-06-20 10:33:44 UTC 120.20.137.139

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(session({
     secret: process.env.SESSION_SECRET || 'a_default_secret_key_for_development',
     resave: false,
+    saveUninitialized: false,
+    cookie: 
 }))
 
 // Middleware
