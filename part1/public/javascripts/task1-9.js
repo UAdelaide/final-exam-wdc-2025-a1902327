f5bafb58be921a7ff13235ddbab01a81e3108dd7
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 07:45:21 +0000

    [automatic commit] 2025-06-20 07:45:21 UTC 120.20.187.84

--- part1/public/javascripts/task1-9.js
+++ part1/public/javascripts/task1-9.js
@@ -47,6 +47,29 @@ createApp({
         }
     },
     mounted() {
-        this.getDog();
+        const xhr = new XMLHttpRequest();
+
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    try {
+                        const data = JSON.parse(xhr.responseText);
+
+                        if (data.status === 'success') {
+                            this.dog_img_link = data.message;
+                        }
+                    } catch (e) {
+                        console.error("Error parsing JSON:", e);
+                    }
+                } else {
+                    console.error("Request failed with status:", xhr.status);
+                }
+            };
+
+            xhr.onerror = () => {
+                console.error("Transfer error");
+            };
+
+            xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+            xhr.send();
     }
 }).mount('#app');
