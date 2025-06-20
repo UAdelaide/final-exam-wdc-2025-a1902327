ae792aaa24da12b09b6b7375e3f8fa109e8d26ee
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 10:53:39 +0000

    [automatic commit] 2025-06-20 10:53:39 UTC 120.20.137.139

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -30,7 +30,7 @@ router.post('/register', async (req, res) => {
 
 router.get('/me', (req, res) => {
   if (req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
+    res.status(401).json({ error: 'Not logged in' });
   }
   res.json(req.session.user);
 });
