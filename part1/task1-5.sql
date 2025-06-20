e9eb2a42be1e770d51cdaab3640587ce9f5bf229
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 03:43:19 +0000

    [automatic commit] 2025-06-20 03:43:19 UTC 120.20.187.84

--- part1/task1-5.sql
+++ part1/task1-5.sql
@@ -3,4 +3,5 @@ USE DogWalkService;
 
 -- Insert five users
 INSERT INTO Users (username, email, password_hash, role) VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner)
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', '')
