3b35456d42298801880e7950a42474cb5a2aec96
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-20 05:27:26 +0000

    [automatic commit] 2025-06-20 05:27:26 UTC 120.20.187.84

--- part1/app.js
+++ part1/app.js
@@ -97,7 +97,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
             d.name AS dog_name,
             wr.requested_time,
             wr.duration_minutes,
-            wr.location
+            wr.location,
             u.username AS owner_username
         FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
