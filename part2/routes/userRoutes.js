9e6c1cee10535cd14fa16a253dffa8796b94f7e2
a1902327 <161268076+a1902327@users.noreply.github.com>
2025-06-21 00:02:13 +0000

    [automatic commit] 2025-06-21 00:02:13 UTC 122.150.236.124

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,6 +64,7 @@ router.get('/dogs', async (req, res) => {
       SELECT dog_id, name FROM Dogs WHERE owner_id = ?`, [ownerID]);
       res.json(dogs);
     } catch (error) {
+      console.error('Error getting owner dogs information:', error);
       
     }
   }
